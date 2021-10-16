e41e3e87ccffcfe675bf524a8ce7e25a3c0709fe
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-16 19:26:38 -0400

    Added firebase to frontend

--- App.js
+++ App.js
@@ -2,10 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Constants from 'expo-constants';
+
+// Initialise firebase project
+import firebase from 'firebase/app';
+
+// Using firebase
+if(firebase.apps.length == 0){
+  firebase.initializeApp(Constants.manifest.web.config.firebase);
+}
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! All I know is pain :')</Text>
       <StatusBar style="auto" />
     </View>
   );
