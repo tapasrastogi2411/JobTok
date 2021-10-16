d26af5f19b204621dc8f4892e1290d912c77b20b
mtuntang <mtuntang@students.cs.ubc.ca>
2021-10-16 16:50:21 -0700

    completed profile header, navbar, global buttons

---
+++ frontend/src/styles/index.js
@@ -0,0 +1,13 @@
+import buttonStyles from "./buttonStyles";
+
+const buttonStyles = StyleSheet.create({
+   grayOutlinedButton: {
+       borderColor: 'lightgray',
+       borderWidth: 1,
+       borderRadius: 4,
+       paddingVertical: 10,
+       paddingHorizontal: 30
+   },
+});
+
+export default buttonStyles;
