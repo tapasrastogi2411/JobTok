fc52f02564061393b49809922e66c1703f0b2a13
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-17 07:37:36 -0400

    Tried to complete and test routing

--- frontend/src/navigation/home/index.js
+++ frontend/src/navigation/home/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text } from 'react-native'
 import styles from '../../screens/auth/styles'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 
-export default function HomeScreen(){
+const Tab = createMaterialBottomTabNavigator();
+const EmptyScreen = () => {
+
+    return <View></View>
+}
+
+export default function HomeScreen() {
 
     return (
-        <View style = {{marginTop: 40}}>
-            <Text>This is my HomeScreen</Text>
-        </View>
+    <Tab.Navigator>
+        <Tab.Screen
+        name = "feed"
+        component = {EmptyScreen}
+        />
+    </Tab.Navigator>
     )
 }
