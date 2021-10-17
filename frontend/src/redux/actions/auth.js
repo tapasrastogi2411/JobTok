c0a8b2c7db37c93e4456c55702409b57c6cc3647
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-17 06:38:57 -0400

    Completed code for User State Listener

--- frontend/src/redux/actions/auth.js
+++ frontend/src/redux/actions/auth.js
@@ -2,9 +2,41 @@
 
 // Import firebase
 import firebase from 'firebase'
+import { USER_STATE_CHANGE } from '../constants'
 require('firebase/auth')
 
-export const login = (email, password)  => new Promise((resolve, reject) => {
+export const userAuthStateListener = () => dispatch => {
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if(user){
+
+            dispatch(getCurrentUserData())
+            
+        }
+        else {
+            dispatch({type: USER_STATE_CHANGE, currentUser: null, loaded: true})
+        }
+    })
+
+}
+
+export const getCurrentUserData = () => {
+
+    firebase.firestore()
+    .collection('user')
+    .doc(firebase.auth.currentUser.uid)
+    .onSnapshot((res) => {
+        
+        if(res.exists){
+            return dispatch({
+                type: USER_STATE_CHANGE,
+                currentUser: res.data(),
+                loaded: true
+            })
+        }
+    })
+}
+export const login = (email, password)  => dispatch => new Promise((resolve, reject) => {
 
     console.log("Print statement 1 for login")
 
@@ -21,7 +53,7 @@ export const login = (email, password)  => new Promise((resolve, reject) => {
 
 // Handling registration now, which is almost similar to login
 
-export const register = (email, password)  => new Promise((resolve, reject) => {
+export const register = (email, password)  => dispatch => new Promise((resolve, reject) => {
 
     console.log("Print statement 1 for register")
 
