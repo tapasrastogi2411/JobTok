15f687f0566c13cfeefe1bdd2ac3ea1611dfe224
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-17 01:14:30 -0400

    Added backend code to add users to a collection

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -7,3 +7,17 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+admin.initializeApp()
+
+const db = admin.firestore()
+
+
+exports.newUser = functions.auth.user().onCreate((user) => {
+
+    return db
+    .collection("user")
+    .doc(user.uid)
+    .create(JSON.parse(JSON.stringify(user)))
+});
