89b82e6775a6ebd21a34011bcc306b8ea2810ffd
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-17 00:03:23 -0400

    some more files I missed to commit for partial authentication

--- frontend/src/components/auth/details/index.js
+++ frontend/src/components/auth/details/index.js
@@ -2,11 +2,38 @@ import React, { useState } from "react";
 import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 import styles from './style'
 import { Feather } from '@expo/vector-icons'
+import {useDispatch} from 'react-redux'
+import { register } from "../../../redux/actions";
 
 export default function AuthDetails({ authPage, setDetailsPage }) {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+
+    // Have to dispatch action
+    const dispatch = useDispatch()
+    const handleLogin = () => {
+        dispatch(login(email, password)).then(() => {
+
+            console.log('Login was literally successfully')
+        })
+        .catch(() => {
+            console.log('Unsuccessful login lol')
+        }) 
+
+    }
+    // Handling the registration
+    const handleRegister = () => {
+        dispatch(register(email, password)).then(() => {
+
+            console.log('REGISTER was literally successfully')
+        })
+        .catch(() => {
+            console.log('Unsuccessful REGISTER lol')
+        }) 
+
+    }
 
     return (
         <View style={styles.container}>
@@ -28,10 +55,12 @@ export default function AuthDetails({ authPage, setDetailsPage }) {
             <TextInput
                 onChangeText={(text) => setPassword(text)}
                 placeholder='Password'
+                secureTextEntry
                 style={styles.textInput}
             />
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+            onPress ={() => authPage == 0 ? handleLogin: handleRegister}>
 
                 <Text style={styles.buttonText}> {authPage == 0 ? 'Sign in' : 'Sign up'}</Text>
             </TouchableOpacity>
---
+++ frontend/src/redux/actions/auth.js
@@ -0,0 +1,37 @@
+// Code for authentication
+
+// Import firebase
+import firebase from 'firebase'
+require('firebase/firebase-auth')
+
+export const login = (email, password) => dispatch => new Promise((resolve, reject) => {
+
+    console.log("Print statement 1 for login")
+
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+        resolve()
+    })
+
+        .catch(() => {
+
+            reject()
+        })
+})
+
+// Handling registration now, which is almost similar to login
+
+export const register = (email, password) => dispatch => new Promise((resolve, reject) => {
+
+    console.log("Print statement 1 for register")
+
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        resolve()
+    })
+
+        .catch(() => {
+
+            reject()
+        })
+})
